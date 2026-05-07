8e4271d5cd00547149600ed2a048f2d21676db21
mr-nikoo <mr.niko0719@gmail.com>
2026-05-07 09:06:55 +0800

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
 
-    <div class="p-6 space-y-6">
+    <div class="p-0 space-y-6">
         <!-- ================= HEADER ================= -->
         @include('pages.partials.page-header')
 
